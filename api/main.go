64c2df99ad4ef999af0820594a8af9a7d93accec
tanaka.takurou <tanaka.takurou@icloud.com>
2020-10-08 07:04:48 +0900

    fix for aws-sdk-go-v2 v0.26.0

--- api/main.go
+++ api/main.go
@@ -13,10 +13,11 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/polly"
-	"github.com/aws/aws-sdk-go-v2/service/s3/s3manager"
+	ptypes "github.com/aws/aws-sdk-go-v2/service/polly/types"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	stypes "github.com/aws/aws-sdk-go-v2/service/s3/types"
 )
 
 type APIResponse struct {
@@ -25,7 +26,7 @@ type APIResponse struct {
 
 type Response events.APIGatewayProxyResponse
 
-var cfg aws.Config
+var s3Client *s3.Client
 var pollyClient *polly.Client
 
 const layout  string = "2006-01-02 15:04"
@@ -67,34 +68,36 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 func synthesizeSpeech(ctx context.Context, message string)(string, error) {
 	t := time.Now()
 	if pollyClient == nil {
-		pollyClient = polly.New(cfg)
+		pollyClient = getPollyClient()
 	}
 
 	input := &polly.SynthesizeSpeechInput{
 		Text:         aws.String(message),
-		TextType:     polly.TextTypeText,
-		VoiceId:      polly.VoiceIdTakumi,
-		LanguageCode: polly.LanguageCodeJaJp,
-		OutputFormat: polly.OutputFormatMp3,
+		TextType:     ptypes.TextTypeText,
+		VoiceId:      ptypes.VoiceIdTakumi,
+		LanguageCode: ptypes.LanguageCodeJaJp,
+		OutputFormat: ptypes.OutputFormatMp3,
 	}
-	req := pollyClient.SynthesizeSpeechRequest(input)
-	res, err := req.Send(ctx)
+	res, err := pollyClient.SynthesizeSpeech(ctx, input)
 	if err != nil {
 		return "", err
 	}
 	buf := new(bytes.Buffer)
-	io.Copy(buf, res.SynthesizeSpeechOutput.AudioStream)
+	io.Copy(buf, res.AudioStream)
 	data := buf.Bytes()
 	contentType := "audio/mp3"
 	filename := t.Format(layout2) + ".mp3"
-	uploader := s3manager.NewUploader(cfg)
-	_, err = uploader.Upload(&s3manager.UploadInput{
-		ACL: s3.ObjectCannedACLPublicRead,
+	if s3Client == nil {
+		s3Client = getS3Client()
+	}
+	input_ := &s3.PutObjectInput{
+		ACL: stypes.ObjectCannedACLPublicRead,
 		Bucket: aws.String(os.Getenv("BUCKET_NAME")),
 		Key: aws.String(filename),
 		Body: bytes.NewReader(data),
 		ContentType: aws.String(contentType),
-	})
+	}
+	_, err = s3Client.PutObject(ctx, input_)
 	if err != nil {
 		return "", err
 	}
@@ -102,13 +105,21 @@ func synthesizeSpeech(ctx context.Context, message string)(string, error) {
 	return url, nil
 }
 
-func init() {
-	var err error
-	cfg, err = external.LoadDefaultAWSConfig()
-	cfg.Region = os.Getenv("REGION")
+func getPollyClient() *polly.Client {
+	return polly.NewFromConfig(getConfig())
+}
+
+func getS3Client() *s3.Client {
+	return s3.NewFromConfig(getConfig())
+}
+
+func getConfig() aws.Config {
+	cfg, err := config.LoadDefaultConfig()
 	if err != nil {
 		log.Print(err)
 	}
+	cfg.Region = os.Getenv("REGION")
+	return cfg
 }
 
 func main() {
